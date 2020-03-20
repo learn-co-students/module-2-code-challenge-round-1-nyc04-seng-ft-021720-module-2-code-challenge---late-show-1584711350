252f1e32ecc84854690f05a927061ab7566239c0
AndrewDAntonio <github email address>
2020-03-20 13:48:04 -0400

    Complete

--- app/models/appearance.rb
+++ app/models/appearance.rb
@@ -3,5 +3,5 @@ class Appearance < ApplicationRecord
     belongs_to :guest
     validates :rating, numericality: {greater_than_or_equal_to: 1}
     validates :rating, numericality: {less_than_or_equal_to: 5}
-    validates :guest_id, uniqueness: true
+    validates :guest_id, uniqueness: {scope: :episode_id}
 end
--- app/models/episode.rb
+++ app/models/episode.rb
@@ -8,12 +8,18 @@ class Episode < ApplicationRecord
     current_appearance.rating
     end
 
-    def avg__appearance_rating
+    def avg_appearance_rating
         total = 0
         sum = 0
         self.appearances.each do |appearance|
             total = sum + appearance.rating
         end
-        total/(self.appearances.length)
+        if total != 0
+            return (total/(self.appearances.length)).to_f
+        else
+            return "There are no ratings yet"
+        end
     end
+
+    
 end
