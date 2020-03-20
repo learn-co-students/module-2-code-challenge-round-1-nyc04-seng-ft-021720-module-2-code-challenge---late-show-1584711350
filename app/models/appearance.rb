51c7d066d80a7270c0de4f7db66e72c340d7662d
96StanleyLee <96stanleylee@gmail.com>
2020-03-20 11:12:17 -0400

    added custom validation messages

--- app/models/appearance.rb
+++ app/models/appearance.rb
@@ -2,8 +2,8 @@ class Appearance < ApplicationRecord
   belongs_to :guest
   belongs_to :episode
 
-  validates :rating, inclusion: 1..5
-  validates_uniqueness_of :guest_id, :scope => :episode_id
+  validates :rating, inclusion: {in: 1..5, message: "%{value} has to be between 1-5!" }
 
+  validates :guest_id, uniqueness: {scope: :episode_id, message: "This guest has already been on this show!"}
 
 end
