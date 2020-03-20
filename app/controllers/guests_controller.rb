c9a41a12bea1fab6e9a5da4942301cc4cf0e8079
96StanleyLee <96stanleylee@gmail.com>
2020-03-20 11:03:48 -0400

    added buttons

--- app/controllers/guests_controller.rb
+++ app/controllers/guests_controller.rb
@@ -6,5 +6,30 @@ class GuestsController < ApplicationController
 
   def show
     @guest = Guest.find(params[:id])
+    @appearances = @guest.appearances.sort_by(&:rating).reverse
+
   end 
+
+  def new 
+    @guest = Guest.new
+  end 
+
+  def create
+    @guest = Guest.create(strong_params)
+    redirect_to @guest
+  end 
+
+  def sort 
+    @guest = Guest.find(params[:id])
+    @appearances = @guest.appearances.sort_by(&:rating)
+    render :show
+  end 
+
+
+
+  private
+
+  def strong_params
+    params.require(:guest).permit(:name, :occupation, :picture_url)
+  end
 end
