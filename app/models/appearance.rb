79a6fb618238c4f015e5a1550ad96b3072d91ad8
schimajo75 <schimmy@gmail.com>
2020-03-20 13:49:17 -0400

    fixed links on guest and episode show pages

--- app/models/appearance.rb
+++ app/models/appearance.rb
@@ -1,4 +1,7 @@
 class Appearance < ApplicationRecord
   belongs_to :guest
   belongs_to :episode
+  
+  validates :rating, :inclusion => {:in => 0..5, :message => "Value should be between 1 and 5"}
+  
 end
