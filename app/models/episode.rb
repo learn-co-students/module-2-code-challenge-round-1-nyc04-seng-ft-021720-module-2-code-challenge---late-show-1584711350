a303b36a7b97fd2ac2fcd160c5a2858956f84298
AndrewDAntonio <github email address>
2020-03-20 11:26:56 -0400

    90 min not done

--- app/models/episode.rb
+++ app/models/episode.rb
@@ -1,2 +1,19 @@
 class Episode < ApplicationRecord
+    has_many :appearances
+    has_many :guests, through: :appearances
+
+
+    def current_appearance_rating
+    current_appearance = Appearance.find_by(episode_id: self.id)
+    current_appearance.rating
+    end
+
+    def avg__appearance_rating
+        total = 0
+        sum = 0
+        self.appearances.each do |appearance|
+            total = sum + appearance.rating
+        end
+        total/(self.appearances.length)
+    end
 end
