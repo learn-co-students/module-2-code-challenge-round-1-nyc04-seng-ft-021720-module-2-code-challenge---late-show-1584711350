427d13dccd575c86ebe3ca7d517fa4d7eef74cbf
Chris Leja <christopher.leja@gmail.com>
2020-03-20 11:16:24 -0400

    first 90 minutes

--- app/models/episode.rb
+++ app/models/episode.rb
@@ -1,2 +1,11 @@
 class Episode < ApplicationRecord
+    has_many :appearances
+    has_many :guests, through: :appearances
+
+    def average_rating
+        
+        @appearances = Appearance.select {|a| a.episode == @episode}
+        total_ratings = @appearances.sum(&:rating) 
+        total_ratings.to_f / @appearances.count 
+    end
 end
